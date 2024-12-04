3316c5e9528b45529924c5d1c7e3f919e3a1c58e
levanthanhlong <thanhtdbn132001@gmail.com>
2024-12-04 19:09:54 +0700

    2

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'hi',
+      home: MainTabBarPage(),
+    );
+  }
+}
+
+class MainTabBarPage extends StatefulWidget {
+  const MainTabBarPage({super.key});
+
+  @override
+  State<MainTabBarPage> createState() => _MainTabBarPageState();
+}
+
+class _MainTabBarPageState extends State<MainTabBarPage> {
+  int selectedIndex = 2;
+
+  static List<Widget> tabBarPages = [
+    const HomePage(),
+    const HomePage(),
+    const HomePage(),
+    const HomePage(),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabBarPages[selectedIndex],
+      bottomNavigationBar: bottomNavigationBar(context),
+    );
+  }
+
+  BottomNavigationBar bottomNavigationBar(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.white,
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                IconlyBold.play,
+              ),
+              label: "Play"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                IconlyBold.video,
+              ),
+              label: "Courses"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                IconlyBold.home,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                IconlyBold.user_2,
+              ),
+              label: "User"),
+        ]);
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
